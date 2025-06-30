b831d3af5a3667411bab64a55bf8151b8c29a41f
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-06-30 20:47:55 +0100

    Migrate changes and Import initial "Workouts" model.

--- workouts/models.py
+++ workouts/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Workouts 
+
 class Workouts(models.Model):
     title = models.CharField()
     content = models.TextField()
     created_on = models.DateTimeField()
+
