4b849003162697e1af5fc57ee29ca79ea536493a
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-07-17 15:03:30 +0100

    Add templates directory and add base.html.

--- workouts/models.py
+++ workouts/models.py
@@ -13,6 +13,7 @@ TRAINING_STYLE = [
     ('timeundertension', 'TIMEUNDERTENSION'),
     ('other', 'OTHER'),
 ]
+
 class Workouts(models.Model):
     title = models.CharField(max_length= 200)
     training_split = models.CharField(max_length= 200, default='Enter Split')
@@ -24,7 +25,8 @@ class Workouts(models.Model):
 class Exercise(models.Model):
     workout = models.ForeignKey(Workouts, on_delete = models.CASCADE) 
     name = models.CharField(max_length= 100)
-    training_style = models.CharField(max_length= 1 , choices= TRAINING_STYLE , default = "split")
+    training_style = models.CharField(max_length= 50 , choices= TRAINING_STYLE , default = "split")
     sets = models.PositiveIntegerField(default=4)
     reps = models.PositiveIntegerField(default=12)
     weight = models.PositiveIntegerField(default=12)
+    units = models.CharField(default= 'kg or lbs')
