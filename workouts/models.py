4f29d16bfa3c927cf1d7b858bc368e8bbc072a94
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-07-22 12:17:12 +0100

    Apply string methods to workout and exercise models.

--- workouts/models.py
+++ workouts/models.py
@@ -20,13 +20,19 @@ class Workouts(models.Model):
     description = models.CharField(max_length= 200, default='Enter Short Description')
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 # Exercise
 
 class Exercise(models.Model):
-    workout = models.ForeignKey(Workouts, on_delete = models.CASCADE) 
+    workout = models.ForeignKey(Workouts, related_name= 'exercises', on_delete = models.CASCADE) 
     name = models.CharField(max_length= 100)
     training_style = models.CharField(max_length= 50 , choices= TRAINING_STYLE , default = "split")
     sets = models.PositiveIntegerField(default=4)
     reps = models.PositiveIntegerField(default=12)
     weight = models.PositiveIntegerField(default=12)
     units = models.CharField(default= 'kg or lbs')
+
+    def __str__(self):
+        return f" {self.name} - {self.sets} x {self.reps}"
