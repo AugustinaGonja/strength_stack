d48a648b5c4fadc671b8c59c1dd50bbf156ce1b6
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-07-24 11:05:53 +0100

    New about page : Update urls and views

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, Dashboard
+from .views import Home, Dashboard , About
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('dashboard/', Dashboard.as_view(), name='dash'),
+    path('about/', About.as_view(), name='about'),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -2,9 +2,12 @@ from django.views.generic import ListView, TemplateView
 from .models import Workouts, Exercise
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = "index.html"
+
 class Dashboard(ListView):
     model = Workouts
     template_name = "dashboard.html"
 
-class Home(TemplateView):
-    template_name = "index.html"
+class About(TemplateView):
+    template_name = "about.html"
