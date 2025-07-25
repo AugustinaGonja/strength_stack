d5a0a900d12beed7dbdcc90a0a8d92dca77d7a9b
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-07-25 18:43:20 +0100

    define choices for gender var in user model. Add bio var and string method,

--- users/models.py
+++ users/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+GENDER = [
+    ('male', 'MALE'),
+    ('female', 'FEMALE'),
+    ('other', 'OTHER'),
+]
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name= 'user', on_delete = models.CASCADE) 
     age = models.PositiveIntegerField(default= 24)
-    gender = models.CharField(max_length= 50 , default = "m/f")
+    gender = models.CharField(max_length= 50 , choices= GENDER ,  default = "m/f")
     current_weight = models.PositiveIntegerField(default=4)
     goal_weight = models.PositiveIntegerField(default=12)
+    bio = models.TextField(default= 'Provide description about yourself and fitness goals.')
 
-    
+    def __str__(self):
+        return f"{self.user}'s Profile"
