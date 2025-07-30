f060df7157fc16eae3c1218b73f5dea469043707
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-07-30 14:08:21 +0100

    add instance = so that form populates with existsing data if any

--- workouts/views.py
+++ workouts/views.py
@@ -46,12 +46,12 @@ def DeleteWorkout(request, workout_id):
 
 def UpdateWorkout(request, workout_id):
    workout = get_object_or_404(Workouts, id=workout_id)
-   form = UpdateWorkoutForm(request.POST)
+   form = UpdateWorkoutForm(request.POST  or None, instance=workout)
    return render (request, "update_workout.html", {"workout": workout,"form": form})
     
 def UpdateExercise(request, exercise_id):
    exercise = get_object_or_404(Exercise, id=exercise_id)
-   form = UpdateExerciseForm(request.POST)
+   form = UpdateExerciseForm(request.POST or None, instance=exercise)
    return render (request, "update_exercise.html", {"exercise": exercise, "form": form})
 
 
