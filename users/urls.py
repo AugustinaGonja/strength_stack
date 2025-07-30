197ac69899756ea0abcdbe46d27de6f00c541cb4
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-07-30 21:13:44 +0100

    create profile page

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 from .import views
 
 urlpatterns = [
-    path('profile/', views.Profile, name='user_profile'),
+    path('profile/', views.UserProfile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Profile
 
 # Create your views here.
-def Profile(request):
+def UserProfile(request):
     profile = get_object_or_404(Profile, user=request.user)
     return render(request, "profile.html", {"profile": profile})
 
