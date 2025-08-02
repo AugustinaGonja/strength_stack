b1bbc1f39faa3b9d5041847209bdf8cce0914f8b
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-08-02 14:38:00 +0100

    Allow user to edit profile page

--- users/forms.py
+++ users/forms.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields =['user', 'age', 'gender','current_weight','goal_weight','bio']
+        fields =['age', 'gender','current_weight','goal_weight','bio']
--- users/views.py
+++ users/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
 def UserProfile(request):
     user_info = Profile.objects.get(user=request.user)
     return render(request, "profile.html", {'user_info': user_info})
 
+
+
+def UpdateProfile(request):
+    user_info = Profile.objects.get(user=request.user)
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=user_info)
+        if form.is_valid():
+            form.save()
+            return redirect ('profile')
+    else:
+        form = ProfileForm(instance=user_info)
+    return render(request, "update_profile.html", {"form": form})
