f94c30cce0f5a58906689b1a4085ecb12a1ddd16
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-08-05 12:40:23 +0100

    create css stylesheet.

--- strengthstack/settings.py
+++ strengthstack/settings.py
@@ -151,9 +151,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIR = (
+STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
