bcccdc58c68a781b7efd1a4d0028d021fe554d33
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-08-05 12:40:43 +0100

    add profile_image

--- users/forms.py
+++ users/forms.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields =['age', 'gender','current_weight','goal_weight','bio','profile_image',]
+        fields =['profile_image','age', 'gender','current_weight','goal_weight','bio',]
