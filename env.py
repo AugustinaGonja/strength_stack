37c38318d95bfd634d196f5be2d1dc1fa8cce544
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-08-10 16:41:11 +0100

    stop tracking

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_z0JUiLeuaKo6@ep-restless-heart-a2psz9lz.eu-central-1.aws.neon.tech/thank_genre_clerk_420562")
-os.environ.setdefault(
-    "SECRET_KEY", "jvhooybuu1$1$obchl5a7iq4nfev52$r-q$(@lz0mj-d%m#su5")
